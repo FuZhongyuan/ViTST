d3375536ecde534bcb7923ebd8638aef49aa2ff3
FuZhongyuan <160452145+FuZhongyuan@users.noreply.github.com>
2024-07-27 14:45:45 +0800

    1

--- dataset/CMAPASS_fzy/processed_data/look_data.py
+++ dataset/CMAPASS_fzy/processed_data/look_data.py
@@ -1,6 +1,6 @@
 import numpy as np
 
 #导入npy文件路径位置
-arr_outcomes = np.load('F:\dachang\ViTST-main\dataset/CMAPASS_fzy/processed_data/arr_outcomes_2.npy', allow_pickle=True)
+arr_outcomes = np.load('..\../CMAPASS_fzy/processed_data/arr_outcomes_2.npy', allow_pickle=True)
 # print(arr_outcomes["Length_of_stay"])
 print(arr_outcomes)
