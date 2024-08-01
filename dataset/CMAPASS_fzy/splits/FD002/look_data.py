fd08823765813d64a2ecf0e331bdf8f41364cda5
FuZhongyuan <160452145+FuZhongyuan@users.noreply.github.com>
2024-08-01 10:52:13 +0800

    Clear Git cache

--- dataset/CMAPASS_fzy/splits/FD002/look_data.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-#导入npy文件路径位置
-arr_outcomes = np.load('./FD002_split1.npy', allow_pickle=True)
-# data = np.load('./PTdict_list_2.npy', allow_pickle=True)
-
-# print(arr_outcomes["Length_of_stay"])
-print(arr_outcomes)
--- dataset/CMAPASS_fzy/splits/look_data.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-#导入npy文件路径位置
-arr_outcomes = np.load('./FD002_split1.npy', allow_pickle=True)
-# print(arr_outcomes["Length_of_stay"])
-print(arr_outcomes)
